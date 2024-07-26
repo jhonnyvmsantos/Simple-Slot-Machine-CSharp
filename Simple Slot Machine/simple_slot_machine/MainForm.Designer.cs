41c24981a5050118d87bcd781e5c5651b0149247
DioneKx <mjhonnyvieira@gmail.com>
2024-07-26 19:22:06 -0300

    Jhonny: Front-Finished

---
+++ Simple Slot Machine/simple_slot_machine/MainForm.Designer.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mjhon
+ * Date: 26/07/2024
+ * Time: 17:52
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace simple_slot_machine
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(623, 338);
+			this.Name = "MainForm";
+			this.Text = "simple_slot_machine";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+		}
+	}
+}
---
+++ Simple Slot Machine/simple_slot_machine/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace simple_slot_machine
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		PictureBox lever = new PictureBox();
+		Timer loading = new Timer();
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			this.BackColor = Color.Green;
+			this.FormBorderStyle = FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			
+			loading.Interval = 100;
+			loading.Tick += TimerTickLoading;
+			
+			lever.Name = "lever"; 
+			lever.Tag = "available";
+			lever.SizeMode = PictureBoxSizeMode.StretchImage;
+			lever.BackgroundImageLayout = ImageLayout.Stretch;
+			lever.Width = 200; lever.Height = 200;
+			lever.BackgroundImage = Image.FromFile("lever.gif");
+			lever.Left = (this.Height / 2) + 225;
+			lever.Top = (this.Height / 2) - 135;
+			lever.Click += LeverClick;
+			lever.Parent = this;
+			
+			for (int i = 0; i < 3; i++)
+			{
+				PictureBox dice = new PictureBox();
+				dice.Name = "dice" + i.ToString(); 
+				dice.SizeMode = PictureBoxSizeMode.StretchImage;
+				dice.Width = 100; dice.Height = 100;
+				dice.Load("interrogation.png");
+				dice.Left = 110 * i + 1 + 50;
+				dice.Top = (this.Height / 2) - 70;
+				dice.Parent = this;
+			}
+		}
+		
+		async void LeverClick(object sender, EventArgs e)
+		{
+
+		}
+		
+		void TimerTickLoading(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
