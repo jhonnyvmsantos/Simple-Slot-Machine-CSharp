59906a41818ee767ec8c900082ed66e94e4baf1b
DioneKx <mjhonnyvieira@gmail.com>
2024-07-26 19:40:14 -0300

    Jhonny: Game-Incomplete

--- Simple Slot Machine/simple_slot_machine/MainForm.cs
+++ Simple Slot Machine/simple_slot_machine/MainForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Media;
 using System.Windows.Forms;
 using System.Threading.Tasks;
 
@@ -13,8 +14,12 @@ namespace simple_slot_machine
 			InitializeComponent();
 		}
 		
+		SoundPlayer rolling = new SoundPlayer("lever.wav");
 		PictureBox lever = new PictureBox();
 		Timer loading = new Timer();
+		Random randomizer = new Random();
+		
+		int count = 0;
 		
 		void MainFormLoad(object sender, EventArgs e)
 		{
@@ -40,6 +45,7 @@ namespace simple_slot_machine
 			{
 				PictureBox dice = new PictureBox();
 				dice.Name = "dice" + i.ToString(); 
+				dice.Tag = i;
 				dice.SizeMode = PictureBoxSizeMode.StretchImage;
 				dice.Width = 100; dice.Height = 100;
 				dice.Load("interrogation.png");
@@ -51,12 +57,46 @@ namespace simple_slot_machine
 		
 		async void LeverClick(object sender, EventArgs e)
 		{
-
+			lever.BackgroundImage = null;
+			rolling.Play();
+			lever.Load("lever.gif");
+			loading.Enabled = true;
+			
+			await Task.Delay(3500);
+			lever.BackgroundImage = Image.FromFile("lever.gif");
+			lever.Image = null;
 		}
 		
 		void TimerTickLoading(object sender, EventArgs e)
 		{
+			count++;
 			
+			if (count < 35)
+			{
+				RollingDices();
+			}
+			else
+			{
+				loading.Enabled = false;
+				count = 0;
+			}
+		}
+		
+		void RollingDices()
+		{
+			foreach (Control control in this.Controls)
+		    {
+				if (control is PictureBox && control.Name.Contains("dice") == true)
+		        {
+		            PictureBox dice = control as PictureBox;
+		            dice.Load("dice" + randomizer.Next(1,7).ToString() + ".png");
+		            
+		            if (Convert.ToInt32(dice.Tag) == 2)
+		            {
+		            	break;
+		            }
+		        }
+		    }
 		}
 	}
 }
