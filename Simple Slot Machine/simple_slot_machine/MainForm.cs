0a7c27d739a2473f2221eb9e2b6089a69d53551b
DioneKx <mjhonnyvieira@gmail.com>
2024-07-26 21:23:26 -0300

    Jhonny: Game-Complete

--- Simple Slot Machine/simple_slot_machine/MainForm.cs
+++ Simple Slot Machine/simple_slot_machine/MainForm.cs
@@ -19,7 +19,14 @@ namespace simple_slot_machine
 		Timer loading = new Timer();
 		Random randomizer = new Random();
 		
-		int count = 0;
+		Label wallet = new Label();
+		Label bonus = new Label();
+		Label cost = new Label();
+		Label reward = new Label();
+		
+		int count = 0, bcount = 0, money = 500, payoff = 250, price = 10, attempts = 0, multiplier = 1;
+		
+		int[] results = new int[3];
 		
 		void MainFormLoad(object sender, EventArgs e)
 		{
@@ -36,10 +43,31 @@ namespace simple_slot_machine
 			lever.BackgroundImageLayout = ImageLayout.Stretch;
 			lever.Width = 200; lever.Height = 200;
 			lever.BackgroundImage = Image.FromFile("lever.gif");
-			lever.Left = (this.Height / 2) + 225;
+			lever.Left = (this.Width / 2) + 115;
 			lever.Top = (this.Height / 2) - 135;
 			lever.Click += LeverClick;
 			lever.Parent = this;
+			
+			wallet.AutoSize = true; LabelTextChanges("wallet");
+			wallet.Font = new Font(FontFamily.GenericSerif, 21f);
+			wallet.Left = 15; wallet.Top = 25;
+			wallet.BorderStyle = BorderStyle.Fixed3D;
+			wallet.Parent = this;
+			
+			bonus.AutoSize = true; LabelTextChanges("bonus");
+			bonus.Font = new Font(FontFamily.GenericSerif, 14f);
+			bonus.Left = 15; bonus.Top = this.Height - 75;
+			bonus.Parent = this;
+			
+			cost.AutoSize = true; LabelTextChanges("cost");
+			cost.Font = new Font(FontFamily.GenericSerif, 17f, FontStyle.Bold);
+			cost.Left = 180; cost.Top = (this.Height / 2) + 50;
+			cost.Parent = this;
+			
+			reward.AutoSize = true; LabelTextChanges("reward");
+			reward.Font = new Font(FontFamily.GenericSerif, 14f);
+			reward.Left = this.Width - 200; reward.Top = 15;
+			reward.Parent = this;
 			
 			for (int i = 0; i < 3; i++)
 			{
@@ -57,21 +85,24 @@ namespace simple_slot_machine
 		
 		async void LeverClick(object sender, EventArgs e)
 		{
-			lever.BackgroundImage = null;
-			rolling.Play();
-			lever.Load("lever.gif");
-			loading.Enabled = true;
-			
-			await Task.Delay(3500);
-			lever.BackgroundImage = Image.FromFile("lever.gif");
-			lever.Image = null;
+			if (loading.Enabled == false)
+			{
+				lever.BackgroundImage = null;
+				rolling.Play();
+				lever.Load("lever.gif");
+				loading.Enabled = true;
+				
+				await Task.Delay(1500);
+				lever.BackgroundImage = Image.FromFile("lever.gif");
+				lever.Image = null;
+			}
 		}
 		
 		void TimerTickLoading(object sender, EventArgs e)
 		{
 			count++;
 			
-			if (count < 35)
+			if (count < 15)
 			{
 				RollingDices();
 			}
@@ -79,6 +110,74 @@ namespace simple_slot_machine
 			{
 				loading.Enabled = false;
 				count = 0;
+				
+				if (results[0] == results[1] && results[0] == results[2])
+				{
+					MessageBox.Show("Parabéns!!! Você conseguiu 3 iguais.\nVocê ganhou R$ " + (payoff).ToString() + ",00.");
+					money += payoff;
+				}
+				else
+				{
+					money -= price;
+					
+					if (money <= 0)
+					{
+						MessageBox.Show("Que pena... Você zerou sua conta.\nBoa sorte na próxima tentativa.");
+						Application.Restart();
+					}					
+				}
+				
+				if (money >= 1000 && money < 3000 && bcount == 0)
+				{
+					MessageBox.Show("Parabéns!!! Você já conseguiu o dobro do valor inicial!\nGaroto(a) de sorte, hein.");
+					bcount++;
+				}
+				else if (money >= 3000)
+				{
+					MessageBox.Show("Okay... Você é bem ganancioso! Está causando nossa falência.\nDito isso, volte para a estaca zero. XD");
+					Application.Restart();
+				}
+				
+				if (attempts == 10 || attempts == 20 || attempts == 30 || attempts == 40)
+				{
+					MessageBox.Show("Ao que parece, você é bem obstinado. Enfim, o multiplcador de recompensa e preço, por tentativa, aumentou em 1!");
+					
+					multiplier += 1;
+					price = 10 * multiplier;
+					payoff = 250 * multiplier;
+					
+					string[] elements = new string[3]
+					{
+						"bonus", "cost", "reward"
+					};
+					
+					foreach (string element in elements)
+					{
+						LabelTextChanges(element);
+					}
+				}
+				
+				attempts++;
+				LabelTextChanges("wallet");
+			}
+		}
+		
+		void LabelTextChanges(string type)
+		{
+			switch (type)
+			{
+				case "wallet":
+					wallet.Text = "Carteira (R$): " + money.ToString() + ",00";
+					break;
+				case "bonus":
+					bonus.Text = "Multiplicador: " + multiplier.ToString() + "x";
+					break;
+				case "cost":
+					cost.Text = "$ " + price.ToString();
+					break;
+				case "reward":
+					reward.Text = "Recompensa: " + payoff.ToString() + ",00";
+					break;
 			}
 		}
 		
@@ -89,9 +188,13 @@ namespace simple_slot_machine
 				if (control is PictureBox && control.Name.Contains("dice") == true)
 		        {
 		            PictureBox dice = control as PictureBox;
-		            dice.Load("dice" + randomizer.Next(1,7).ToString() + ".png");
+		            int number = randomizer.Next(1,7);
+		            int tag = Convert.ToInt32(dice.Tag);
 		            
-		            if (Convert.ToInt32(dice.Tag) == 2)
+		            dice.Load("dice" + number.ToString() + ".png");
+		            results[tag] = number;
+		            
+		            if (tag == 2)
 		            {
 		            	break;
 		            }
